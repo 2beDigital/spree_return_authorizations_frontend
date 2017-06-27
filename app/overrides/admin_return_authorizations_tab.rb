4b71c51ecb772e2fd5d84c143ca8ecba98aeeccc
Noel <noel@2bedigital.com>
2017-06-27 11:41:01 +0100

    added missing translate

--- app/overrides/admin_return_authorizations_tab.rb
+++ app/overrides/admin_return_authorizations_tab.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path: "spree/admin/shared/_menu",
   name: "admin_return_authorizations_tab",
   insert_bottom: "[data-hook='admin_tabs']",
-  text: "<%= tab :returns, url: admin_return_authorization_requests_path, icon: 'reply-all' %>"
+  text: "<%= tab Spree.t(:returns), url: admin_return_authorization_requests_path, icon: 'reply-all' %>"
 )
